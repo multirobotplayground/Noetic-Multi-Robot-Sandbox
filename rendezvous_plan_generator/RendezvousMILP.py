e0acb50f5b328df5dd9ea70a6c2f89078e3f8f6e
Alysson Ribeiro da Silva <alysson.ribeiro.silva@gmail.com>
2025-10-20 22:26:40 -0300

    License fix (#15)
    * Fix terms
    
    * Update legacy notice:

--- rendezvous_plan_generator/RendezvousMILP.py
+++ rendezvous_plan_generator/RendezvousMILP.py
@@ -1,3 +1,19 @@
+# Job-shop Scheduling for Multi-robot Intermittent Communication Generator
+# Copyright (C) 2025 Alysson Ribeiro da Silva
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 from mip import *
 from PlotPlan import PlotChart
